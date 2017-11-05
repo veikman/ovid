ed1e1832301aab3da370e1b1bf6bb0f7d7ae0eaa
Viktor Eikman <viktor.eikman@gmail.com>
2017-11-05 12:41:53 +0100

    Improved regex flag support.
    Each Ovid processor now takes a re module ‘flags’ argument to the
    initializer, and this is passed to re.compile for that processor.
    There is likewise a new ‘flags’ property on DelimitedShorthand to
    serve in the compilation of class-wide regexes. The latter change
    replaces the practise of passing a ‘flags’ argument to
    collective_sub(), something that was never fully supported anyway.
    Any such argument will now be ignored in silence.
    
    Taken together, these changes allow for easy multi-line shorthand.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Ovid',
-    version='0.4.1',
+    version='0.5.0',
     description='Text metamorphosis toolbox',
     requires=[],
     author='Viktor Eikman',
