8b7e18f4ac136bbc439d6f29978838dba8747459
Viktor Eikman <viktor.eikman@gmail.com>
2021-04-17 15:14:42 +0200

    Converted test_producing to pytest.

--- ovid/test_producing.py
+++ ovid/test_producing.py
@@ -2,15 +2,20 @@
 """Unit tests for the producing module."""
 
 
-import unittest
 import unittest.mock as mock
 
-from . import basic
-from . import producing
+from ovid.basic import OneWayProcessor
+from ovid.producing import TwoWayProcessor
+from ovid.producing import TwoWaySignatureShorthand
 
 
-class SubstitutionElements(unittest.TestCase):
-    def _forwards(self, regex, *args, **kwargs):
+#######################
+# TESTS OF PRODUCTION #
+#######################
+
+
+def _twoway(regex, reference_output, *args, **kwargs):
+    def _forwards(regex, *args, **kwargs):
         """Examine a generated substitution function signature."""
         m = mock.Mock()
 
@@ -18,68 +23,81 @@ class SubstitutionElements(unittest.TestCase):
             m(*args, **kwargs)
             return ''
 
-        processor = basic.OneWayProcessor(regex, f)
+        processor = OneWayProcessor(regex, f)
         processor.sub('abc')
         m.assert_called_once_with(*args, **kwargs)
 
-    def _backwards(self, regex, reference_output, *args, **kwargs):
+    def _backwards(regex, reference_output, *args, **kwargs):
         """Examine the matchable product of reverse operation."""
-        processor = producing.TwoWayProcessor(regex, lambda: None)
-        self.assertEqual(reference_output, processor.produce(*args, **kwargs))
+        processor = TwoWayProcessor(regex, lambda: None)
+        assert reference_output == processor.produce(*args, **kwargs)
 
-    def _twoway(self, regex, reference_output, *args, **kwargs):
-        self._forwards(regex, *args, **kwargs)
-        self._backwards(regex, reference_output, *args, **kwargs)
-
-    def test_empty(self):
-        self._twoway('$', '$')
-
-    def test_no_groups(self):
-        self._twoway('a', 'a')
-
-    def test_unnamed_group_multicharacter(self):
-        self._twoway('(ab)', 'ab', 'ab')
-
-    def test_unnamed_groups(self):
-        self._twoway('(a)(b)', 'ab', *'ab')
-
-    def test_named_group(self):
-        self._twoway('(?P<n0>a)', 'a', n0='a')
-
-    def test_named_groups(self):
-        self._twoway('(?P<n0>a)b(?P<n1>c)', 'abc', n0='a', n1='c')
-
-    def test_mix(self):
-        self._twoway('(?P<n0>a)(b)(?P<n1>c)', 'abc', 'b', n0='a', n1='c')
+    _forwards(regex, *args, **kwargs)
+    _backwards(regex, reference_output, *args, **kwargs)
 
 
-class SignatureShorthandProduction(unittest.TestCase):
-    def test_production_noargs(self):
-        def h():
-            return '手'
+def test_empty():
+    _twoway('$', '$')
 
-        p = producing.TwoWaySignatureShorthand(h)
-        self.assertEqual(p.produce(), '{{h}}')
-        self.assertEqual(p.sub(p.produce()), '手')
 
-    def test_production_unnamed(self):
-        def a(v):
-            raise NotImplementedError
+def test_no_groups():
+    _twoway('a', 'a')
 
-        p = producing.TwoWaySignatureShorthand(a)
-        self.assertEqual(p.produce(1), '{{a|1}}')
 
-    def test_production_named(self):
-        def a(v=None):
-            raise NotImplementedError
+def test_unnamed_group_multicharacter():
+    _twoway('(ab)', 'ab', 'ab')
 
-        p = producing.TwoWaySignatureShorthand(a)
-        self.assertEqual(p.produce(v=1), '{{a|v=1}}')
 
-    def test_production_combined(self):
-        def a(v0, v1=None):
-            raise NotImplementedError
+def test_unnamed_groups():
+    _twoway('(a)(b)', 'ab', *'ab')
 
-        p = producing.TwoWaySignatureShorthand(a)
-        self.assertEqual(p.produce(1, v1=2), '{{a|1|v1=2}}')
-        self.assertEqual(p.produce(1), '{{a|1}}')
+
+def test_named_group():
+    _twoway('(?P<n0>a)', 'a', n0='a')
+
+
+def test_named_groups():
+    _twoway('(?P<n0>a)b(?P<n1>c)', 'abc', n0='a', n1='c')
+
+
+def test_mix():
+    _twoway('(?P<n0>a)(b)(?P<n1>c)', 'abc', 'b', n0='a', n1='c')
+
+
+#####################################
+# TESTS OF TwoWaySignatureShorthand #
+#####################################
+
+
+def test_production_noargs():
+    def h():
+        return '手'
+
+    p = TwoWaySignatureShorthand(h)
+    assert p.produce() == '{{h}}'
+    assert p.sub(p.produce()) == '手'
+
+
+def test_production_unnamed():
+    def a(v):
+        raise NotImplementedError
+
+    p = TwoWaySignatureShorthand(a)
+    assert p.produce(1) == '{{a|1}}'
+
+
+def test_production_named():
+    def a(v=None):
+        raise NotImplementedError
+
+    p = TwoWaySignatureShorthand(a)
+    assert p.produce(v=1) == '{{a|v=1}}'
+
+
+def test_production_combined():
+    def a(v0, v1=None):
+        raise NotImplementedError
+
+    p = TwoWaySignatureShorthand(a)
+    assert p.produce(1, v1=2) == '{{a|1|v1=2}}'
+    assert p.produce(1) == '{{a|1}}'
